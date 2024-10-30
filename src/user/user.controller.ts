17c593d7dfff84b6d2f071da7530ad36a7c7ed00
José García <jofagaca69@gmail.com>
2024-10-30 14:16:53 -0500

    Feat: Creación de lógica para agregar usuarios, además de conexión entre webservice y base de datos mySQL

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,39 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpException, HttpStatus, Inject, Param } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Controller('user')
 export class UserController {
+    constructor(@Inject('USER_SERVICE') private readonly userServiceClient: ClientProxy) {}
 
-    constructor(@Inject('USER_SERVICE') private readonly userServiceClient: ClientProxy) {
+    @Get(':id')
+    getUser(@Param('id') id: number): Observable<any> {
+        return this.userServiceClient.send({ cmd: 'get_user' }, { id }).pipe(
+            catchError(err => {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.NOT_FOUND,
+                        error: `User with ID ${id} not found`,
+                    },
+                    HttpStatus.NOT_FOUND,
+                );
+            }),
+        );
     }
 
-    @Get()
-    getUser(): Observable<any>{
-        return this.userServiceClient.send({ cmd: 'get_user' }, { id: 1 })
+    @Post()
+    createUser(@Body() userData: { name: string; lastname: string; email: string; phone?: string }): Observable<any> {
+        return this.userServiceClient.send({ cmd: 'create_user' }, userData).pipe(
+            catchError(err => {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.CONFLICT,
+                        error: `User with email ${userData.email} already exists`,
+                    },
+                    HttpStatus.CONFLICT,
+                );
+            }),
+        );
     }
 }
